e3c6de173f5e188f675bfa253ddf20c2c24142fd
Leonid Petrov <lp.msarchitect@gmail.com>
2020-06-16 00:23:01 +0300

    feat: add create and login user functions

---
+++ src/components/authorization/clientApi.js
@@ -0,0 +1,51 @@
+export const createUser = async (user) => {
+  try {
+    const rawResponse = await fetch(
+      'https://afternoon-falls-25894.herokuapp.com/users',
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+      }
+    );
+
+    if (rawResponse.status === 422) {
+      throw new Error('Incorrect e-mail or password');
+    }
+
+    const content = await rawResponse.json();
+
+    return content;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const loginUser = async (user) => {
+  try {
+    const rawResponse = await fetch(
+      'https://afternoon-falls-25894.herokuapp.com/signin',
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+      }
+    );
+
+    if (rawResponse.status === 403) {
+      throw new Error('Incorrect e-mail or password');
+    }
+
+    const content = await rawResponse.json();
+
+    return content;
+  } catch (error) {
+    console.error(error);
+  }
+};
