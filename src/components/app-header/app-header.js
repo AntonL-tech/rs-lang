bd03c546ddac14f76a70629776cac5374974d695
Anton <antonlashyk@gmail.com>
2020-06-16 23:31:09 +0300

    feat: add burger

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import s from './app-header.module.css'
+import Burger from '../header-burger/header-burger'
 
 function Header() {
     return (
         <div className={s.header_inner}>
+            <Burger/>
             <div className={s.header_logo}>RSLang</div>
             <div className={s.header_user}>User</div>
             <button className={s.header_button}>Logout</button>
--- src/components/app-sidebar/app-sidebar.js
+++ src/components/app-sidebar/app-sidebar.js
@@ -2,17 +2,16 @@ import React from 'react';
 import a from './app-sidebar.module.css'
 import {Link} from "react-router-dom";
 
-function Sidebar() {
+
+const Sidebar = () => {
     return (
-        <div className={a.sidebar_inner}>
-            <ul className={a.sidebar_list}>
-                <li className={a.sidebar_item}><Link to='/app-settings'><i className="fas fa-cog fa-5x"/></Link></li>
-                <li className={a.sidebar_item}><Link to='/app-games'><i className="fas fa-gamepad fa-5x"/></Link></li>
-                <li className={a.sidebar_item}><Link to='/app-words'><i className="fas fa-book-dead fa-5x"/></Link></li>
-                <li className={a.sidebar_item}><Link to='/app-stats'><i className="fas fa-grin-stars fa-5x"/></Link></li>
-                <li className={a.sidebar_item}><Link to='/app-team'><i className="fas fa-users fa-5x"/></Link></li>
-            </ul>
-        </div>
+        <ul className={a.sidebar_list}>
+            <li className={a.sidebar_item}><Link to='/app-settings'><i className="fas fa-cog fa-5x"/></Link></li>
+            <li className={a.sidebar_item}><Link to='/app-games'><i className="fas fa-gamepad fa-5x"/></Link></li>
+            <li className={a.sidebar_item}><Link to='/app-words'><i className="fas fa-book-dead fa-5x"/></Link></li>
+            <li className={a.sidebar_item}><Link to='/app-stats'><i className="fas fa-grin-stars fa-5x"/></Link></li>
+            <li className={a.sidebar_item}><Link to='/app-team'><i className="fas fa-users fa-5x"/></Link></li>
+        </ul>
     )
 }
 export default Sidebar;
