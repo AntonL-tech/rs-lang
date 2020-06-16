e3263f89a49f412bf48b82f0849d95848e549139
Anton <antonlashyk@gmail.com>
2020-06-16 19:28:23 +0300

    feat: add new item to sidebar

--- src/components/app-sidebar/app-sidebar.js
+++ src/components/app-sidebar/app-sidebar.js
@@ -8,6 +8,7 @@ function Sidebar() {
                 <li className={a.sidebar_item}><a href="#"><i className="fas fa-cog fa-5x"/></a></li>
                 <li className={a.sidebar_item}><a href="#"><i className="fas fa-gamepad fa-5x"/></a></li>
                 <li className={a.sidebar_item}><a href="#"><i className="fas fa-book-dead fa-5x"/></a></li>
+                <li className={a.sidebar_item}><a href="#"><i className="fas fa-grin-stars fa-5x"/></a></li>
                 <li className={a.sidebar_item}><a href="#"><i className="fas fa-users fa-5x"/></a></li>
             </ul>
         </div>
