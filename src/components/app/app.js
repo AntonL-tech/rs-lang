e0696aa658944d83ad77acdff0e435331fd311b1
Leonid Petrov <lp.msarchitect@gmail.com>
2020-06-17 20:18:24 +0300

    feat: plug auth

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import s from './app.module.css'
+import s from './app.module.css';
+import Auth from '../Auth';
+
+function onAuth(user) {
+  console.log(user);
+}
 
 function App() {
-    return (
-        <>
-            <h1 className={s.title}>Hello World</h1>
-        </>
-    );
+  return (
+    <>
+      <h1 className={s.title}>Hello World</h1>
+      <Auth onAuth={onAuth} />
+    </>
+  );
 }
 export default App;
