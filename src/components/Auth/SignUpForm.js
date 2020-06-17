32a23953a2edd20f07f1086eff4fbbf2b177245b
Leonid Petrov <lp.msarchitect@gmail.com>
2020-06-17 19:03:14 +0300

    feat: add confirm password

--- src/components/Auth/SignUpForm.js
+++ src/components/Auth/SignUpForm.js
@@ -6,6 +6,8 @@ class SignUpForm extends Component {
     this.state = {
       email: '',
       password: '',
+      confirmPassword: '',
+      noConfirmText: '',
     };
 
     this.onChangeValue = this.onChangeValue.bind(this);
@@ -19,11 +21,20 @@ class SignUpForm extends Component {
     if (e.target.name === 'password') {
       this.setState({ password: e.target.value });
     }
+    if (e.target.name === 'confirm-password') {
+      this.setState({ confirmPassword: e.target.value });
+    }
   }
 
   async onSubmit(e) {
     e.preventDefault();
-    this.props.onSubmit(this.state);
+    if (this.state.password === this.confirmPassword) {
+      this.props.onSubmit(this.state);
+    } else {
+      this.setState({
+        noConfirmText: 'Password not match',
+      });
+    }
   }
 
   render() {
@@ -41,6 +52,13 @@ class SignUpForm extends Component {
           placeholder="Password"
           onChange={this.onChangeValue}
         />
+        <input
+          type="password"
+          name="confirm-password"
+          placeholder="Confirm Password"
+          onChange={this.onChangeValue}
+        />
+        <span>{this.state.noConfirmText}</span>
         <button type="submit">SignUp</button>
       </form>
     );
