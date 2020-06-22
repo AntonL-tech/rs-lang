e6cadc4630583f94b58fbc62256efd6238f2e42a
Leonid Petrov <lp.msarchitect@gmail.com>
2020-06-22 23:23:12 +0300

    feat: add logout button in header

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import s from './app-header.module.css'
+import s from './app-header.module.css';
+import Logout from '../Logout';
 
 function Header() {
-    return (
-        <div className={s.header_inner}>
-            <div className={s.header_logo}>RSLang</div>
-            <div className={s.header_user}>User</div>
-            <button className={s.header_button}>Logout</button>
-        </div>
-    )
+  return (
+    <div className={s.header_inner}>
+      <div className={s.header_logo}>RSLang</div>
+      <div className={s.header_user}>User</div>
+      <Logout className={s.header_button} text="Logout" />
+    </div>
+  );
 }
 export default Header;
