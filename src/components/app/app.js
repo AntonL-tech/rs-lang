91dc1544d7bb82d826f7077709a6026278953ea0
Leonid Petrov <lp.msarchitect@gmail.com>
2020-06-22 12:36:57 +0300

    init: start speakit

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import s from './app.module.css'
+import s from './app.module.css';
+import SpeakIt from '../speakit';
 
 function App() {
-    return (
-        <>
-            <h1 className={s.title}>Hello World</h1>
-        </>
-    );
+  return (
+    <>
+      <h1 className={s.title}>Hello World</h1>
+      <SpeakIt />
+    </>
+  );
 }
 export default App;
---
+++ src/components/speakit/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SpeakIt extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div>SpeakIt!</div>;
+  }
+}
+
+export default SpeakIt;
