17d4d4c78196789f94cc189fa4e83804e4a22d45
Leonid Petrov <lp.msarchitect@gmail.com>
2020-06-24 17:10:32 +0300

    refactor: move regexps in separate variables

--- src/components/Auth/SignUpForm.js
+++ src/components/Auth/SignUpForm.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 import s from './auth.module.css';
 
+// RegExp for validate email
+const EMAIL = /^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i;
+//RegExp for validate password:
+// The password must contain:
+// - at least 8 characters
+// - at least one uppercase letter,
+// - at least one uppercase letter,
+// - at least one number
+// - at least  one special character from + -_ @ $!% *? & #.,;: [] {}
+const PASSWORD = /(?=.*[0-9])(?=.*[+-_@$!%*?&#.,;:[]{}])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z+-_@$!%*?&#.,;:[]{}]{8,}/g;
+
 class SignUpForm extends Component {
   constructor(props) {
     super(props);
@@ -35,17 +46,13 @@ class SignUpForm extends Component {
       });
       return;
     }
-    if (!this.state.email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i)) {
+    if (!this.state.email.match(EMAIL)) {
       this.setState({
         errorText: 'Not valid email',
       });
       return;
     }
-    if (
-      !this.state.password.match(
-        /(?=.*[0-9])(?=.*[+-_@$!%*?&#.,;:[]{}])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z+-_@$!%*?&#.,;:[]{}]{8,}/g
-      )
-    ) {
+    if (!this.state.password.match(PASSWORD)) {
       this.setState({
         errorText: 'Not valid password',
       });
