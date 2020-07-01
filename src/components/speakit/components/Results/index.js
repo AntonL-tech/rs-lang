8c525ef3846d332d14adeb9e9fcaf4b8f045a30f
Leonid Petrov <lp.msarchitect@gmail.com>
2020-07-01 14:58:11 +0300

    feat: add styles to results window

--- src/components/speakit/components/Results/index.js
+++ src/components/speakit/components/Results/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import WordList from './WordList';
+import s from './Results.module.css';
 
 class Results extends Component {
   constructor(props) {
@@ -20,22 +21,24 @@ class Results extends Component {
     let { errorsWords, knownWords, onReturn, onNewGame } = this.props;
 
     return (
-      <div>
+      <div className={s.resultsWrapper}>
         {this.state.audio ? <audio autoPlay src={this.state.audio} /> : null}
-        <section>
-          <div>
-            Ошибок<span>{errorsWords.length}</span>
-          </div>
-          <WordList list={errorsWords} onClick={this.playSound} />
-        </section>
-        <section>
-          <div>
-            Знаю<span>{knownWords.length}</span>
-          </div>
-          <WordList list={knownWords} onClick={this.playSound} />
-        </section>
-        <button onClick={onReturn}>Return</button>
-        <button onClick={onNewGame}>NewGame</button>
+        <div className={s.resultsContainer}>
+          <section>
+            <div>
+              Ошибок<span>{errorsWords.length}</span>
+            </div>
+            <WordList list={errorsWords} onClick={this.playSound} />
+          </section>
+          <section>
+            <div>
+              Знаю<span>{knownWords.length}</span>
+            </div>
+            <WordList list={knownWords} onClick={this.playSound} />
+          </section>
+          <button onClick={onReturn}>Return</button>
+          <button onClick={onNewGame}>NewGame</button>
+        </div>
       </div>
     );
   }
