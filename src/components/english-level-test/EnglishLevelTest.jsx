c78e5b7af99356dac5d5398f99457b602d23e119
Maksmir <39415242+Maksmir@users.noreply.github.com>
2020-07-08 20:08:33 +0300

    feat: work on the test

---
+++ src/components/english-level-test/EnglishLevelTest.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './EnglishLevelTest.module.css';
+
+
+const EnglishLevelTest = () => {
+    return <div className={s.block}>
+            <div className={s.startButton}>Take an English proficiency test</div>
+            </div>
+}
+export default EnglishLevelTest;
