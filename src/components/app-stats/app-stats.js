caf2f3b6143506b68690685918a400738002cc41
Leonid Petrov <lp.msarchitect@gmail.com>
2020-07-11 16:46:55 +0300

    feat: add bar statistic chart

--- src/components/app-stats/app-stats.js
+++ src/components/app-stats/app-stats.js
@@ -1,18 +1,50 @@
 import React from 'react';
-import s from './app-stats.module.css'
-import Header from '../app-header/app-header'
-import Sidebar from '../app-sidebar/app-sidebar'
-
+import s from './app-stats.module.css';
+import Header from '../app-header/app-header';
+import Sidebar from '../app-sidebar/app-sidebar';
+import BarChart from './BarChart';
 
 function Stats() {
-    return (
-        <div>
-            <Header/>
-            <div className={'flex'}>
-                    <Sidebar/>
-                    <p>Statistic</p>
-            </div>
-        </div>
-    )
+  const barData = {
+    label: 'Mini game name',
+    color: '#70CAD1',
+    data: [
+      {
+        name: 'A',
+        value: 46,
+      },
+      {
+        name: 'B',
+        value: 87,
+      },
+      {
+        name: 'C',
+        value: 97,
+      },
+      {
+        name: 'D',
+        value: 7,
+      },
+      {
+        name: 'E',
+        value: 10,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header />
+      <div className={'flex'}>
+        <Sidebar />
+        <p>Statistic</p>
+        <BarChart
+          data={barData.data}
+          label={barData.label}
+          color={barData.color}
+        />
+      </div>
+    </div>
+  );
 }
 export default Stats;
