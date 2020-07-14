ce6ad4f78997fc9c532b9b5f0a96d88ad594504d
Leonid Petrov <lp.msarchitect@gmail.com>
2020-07-14 21:44:09 +0300

    add charts

--- src/components/app-stats/app-stats.js
+++ src/components/app-stats/app-stats.js
@@ -4,47 +4,43 @@ import s from './app-stats.module.css';
 // import Sidebar from '../app-sidebar/app-sidebar'
 import Page from '../app-page-structure/app-page-structure';
 import BarChart from './BarChart';
+import LineChart from './LineChart';
 import { getUserStatistic, updateUserStatistic } from './statisticApi';
 
 function Stats() {
-  const barData = {
-    label: 'Mini game name',
-    color: '#70CAD1',
-    data: [
-      {
-        name: 'A',
-        value: 46,
-      },
-      {
-        name: 'B',
-        value: 87,
-      },
-      {
-        name: 'C',
-        value: 97,
-      },
-      {
-        name: 'D',
-        value: 7,
-      },
-      {
-        name: 'E',
-        value: 10,
-      },
-    ],
-  };
+  // const userId = localStorage.getItem('userId');
+  // const token = localStorage.getItem('token');
 
-  const userId = localStorage.getItem('userId');
-  const token = localStorage.getItem('token');
+  // const stats = {
+  //   gameName: 'game name',
+  //   correct: 10,
+  //   series: 2,
+  // };
 
-  const stats = {
-    gameName: 'game name',
-    correct: 10,
-    series: 2,
-  };
+  // updateUserStatistic(userId, token, stats).then(() => {
+  //   getUserStatistic(userId, token).then((res) => console.log(res));
+  // });
 
-  updateUserStatistic(userId, token, stats).then(() => {
-    getUserStatistic(userId, token).then((res) => console.log(res));
+  function getRandomDateArray(numItems) {
+    // Create random array of objects (with date)
+    let data = [];
+    let baseTime = new Date('2018-05-01T00:00:00').getTime();
+    let dayMs = 24 * 60 * 60 * 1000;
+    for (var i = 0; i < numItems; i++) {
+      data.push({
+        time: new Date(baseTime + i * dayMs),
+        value: Math.round(20 + 80 * Math.random()),
+      });
+    }
+    return data;
+  }
+
+  let data = [];
+
+  data.push({
+    title: 'Visits',
+    data: getRandomDateArray(150),
+    color: '#3E517A',
   });
 
   return (
@@ -53,11 +49,22 @@ function Stats() {
       <div className={'flex'}>
         {/* <Sidebar/> */}
         <p>Statistic</p>
-        <BarChart
-          data={barData.data}
-          label={barData.label}
-          color={barData.color}
-        />
+        <div className={s.chartsWrapper}>
+          <div className={`${s.main} ${s.chartWrapper}`}>
+            <LineChart
+              data={data[0].data}
+              title={data[0].title}
+              color={data[0].color}
+            />
+          </div>
+          <div className={`${s.sub} ${s.chartWrapper}`}>
+            <BarChart
+              data={data[0].data}
+              title={data[0].title}
+              color={data[0].color}
+            />
+          </div>
+        </div>
       </div>
     </Page>
   );
